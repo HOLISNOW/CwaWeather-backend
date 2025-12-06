d5df2726ed631fc1d581df8a8297f8e50c8eb12f
HOLISNOW <j2kkdc@gmail.com>
2025-12-07 03:14:56 +0800

    fix:臺北市

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 /**
- * 取得台北市天氣預報
+ * 取得臺北市天氣預報
  * CWA 氣象資料開放平臺 API
  * 使用「一般天氣預報-今明 36 小時天氣預報」資料集
  */
@@ -37,7 +37,7 @@ const getKaohsiungWeather = async (req, res) => {
       {
         params: {
           Authorization: CWA_API_KEY,
-          locationName: "桃園市",
+          locationName: "臺北市",
         },
       }
     );
@@ -48,7 +48,7 @@ const getKaohsiungWeather = async (req, res) => {
     if (!locationData) {
       return res.status(404).json({
         error: "查無資料",
-        message: "無法取得台北市天氣資料",
+        message: "無法取得臺北市天氣資料",
       });
     }
 
